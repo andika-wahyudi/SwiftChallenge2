53717558e6b1300e5ecbdbc265c10dee82b2a42e
Andika Wahyudi <andika.aryowahyudi@gmail.com>
2026-07-11 14:58:11 +0800

    Initial Commit

---
+++ SwiftChallenge2/SwiftChallenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChallenge2App.swift
+//  SwiftChallenge2
+//
+//  Created by Andika Wahyudi on 11/7/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChallenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
