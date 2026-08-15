fd52f4cb189cfcb12777799e3e206f3827314daa
Serene <personalzbx@gmail.com>
2026-08-15 17:12:07 +0800

    finished!!!!!!!

--- SwiftChallenge2/Clicker/ClickerView.swift
+++ SwiftChallenge2/Clicker/ClickerView.swift
@@ -12,59 +12,70 @@ struct ClickerView: View {
     
     // everytime you want to put something on screen that changes
     // you need a state variable.
-    @State private var counter = 0
+    @AppStorage("counter1") private var counter = 0
     @State private var message = ""
     @State private var showLeaderboard = false
     @AppStorage("playerName") private var playerName = ""
     @State private var showNamePrompt = false
-    @State private var highscore = 0
+    @AppStorage("HighScore") private var highscore = 0
     @ObservedObject var scoreManager: ScoreManager
     
     
     var body: some View {
-        VStack {
-            Button("Leaderboard") {
-                showLeaderboard = true
-            }
-            .buttonStyle(.glassProminent)
-            
-            Text("Highscore: \(scoreManager.scores.first?.yourScore ?? 0)")
-            Spacer()
-            
-            Image(.jiachen)
-                .resizable()
-                .frame(width: CGFloat(100+2*counter), height: CGFloat(100+2*counter))
+        NavigationStack{
+            ZStack{
+                Image(.jiachen)
+                    .resizable()
+                    .frame(width: CGFloat(100+2*counter), height: CGFloat(100+2*counter))
+                VStack {
+                    Button("Leaderboard") {
+                        showLeaderboard = true
+                    }
+                    .buttonStyle(.glassProminent)
+                    
+                    Text("Highscore: \(highscore)")
+                        .shadow(color:.white, radius: 1, x: 1, y:1)
+                        .shadow(color:.white, radius: 1, x: -1, y:1)
+                        .shadow(color:.white, radius: 1, x: 1, y: -1)
+                        .shadow(color:.white, radius: 1, x: -1, y:-1)
+                    
+                    Spacer()
+                    
+                    Text("Swift progress: \(counter)")
+                        .shadow(color:.white, radius: 1, x: 1, y:1)
+                        .shadow(color:.white, radius: 1, x: -1, y:1)
+                        .shadow(color:.white, radius: 1, x: 1, y: -1)
+                        .shadow(color:.white, radius: 1, x: -1, y:-1)
+                    
+                    Button("I love Swift") {
+                        counter += 1
+                        if highscore < counter {
+                            highscore = counter
+                            scoreManager.addNote(playerName: playerName, yourScore: highscore)
+                        }
+                    }
+                    .buttonStyle(.glassProminent)
+                    NavigationLink("Switch to fake player", destination: SecondPlayerView(scoreManager: ScoreManager()))
+                }
+                .sheet(isPresented: $showLeaderboard) {
+                    ClickerLeaderboardView(scoreManager: ScoreManager())
+                }
                 .padding()
-            
-            Spacer()
-            
-            Text("Swift progress: \(counter)")
-            
-            Button("I love Swift") {
-                counter += 1
-                if scoreManager.scores.first?.yourScore ?? 0 < counter {
-                    highscore = counter
-                    scoreManager.addNote(playerName: playerName, yourScore: highscore)
+                .onAppear {
+                    scoreManager.getNotes()
+                    if playerName.isEmpty {
+                        showNamePrompt = true
+                    }
+                }
+                .alert("Enter your name", isPresented: $showNamePrompt) {
+                    TextField("Your name", text: $playerName) // saves to @AppStorage as they type
+                    Button("Done") { showNamePrompt = false }
                 }
             }
-                .buttonStyle(.glassProminent)
-        }
-        .sheet(isPresented: $showLeaderboard) {
-            ClickerLeaderboardView(scoreManager: ScoreManager())
-        }
-        .padding()
-        .onAppear {
-            scoreManager.getNotes()
-            if playerName.isEmpty {
-                showNamePrompt = true
-            }
-        }
-        .alert("Enter your name", isPresented: $showNamePrompt) {
-            TextField("Your name", text: $playerName) // saves to @AppStorage as they type
-            Button("Done") { showNamePrompt = false }
         }
     }
 }
+
 
 #Preview {
     ClickerView(scoreManager: ScoreManager())
--- SwiftChallenge2/Clicker/SecondPlayerView.swift
+++ SwiftChallenge2/Clicker/SecondPlayerView.swift
@@ -8,65 +8,73 @@
 
 import SwiftUI
 
-struct ClickerView: View {
+struct SecondPlayerView: View {
     
     // everytime you want to put something on screen that changes
     // you need a state variable.
-    @State private var counter = 0
+    @AppStorage("counter2") private var counter = 0
     @State private var message = ""
     @State private var showLeaderboard = false
-    @AppStorage("playerName") private var playerName = ""
+    @AppStorage("Player2") private var playerName = ""
     @State private var showNamePrompt = false
-    @State private var highscore = 0
+    @AppStorage("HighScore2") private var highscore = 0
     @ObservedObject var scoreManager: ScoreManager
     
     
     var body: some View {
-        VStack {
-            Button("Leaderboard") {
-                showLeaderboard = true
-            }
-            .buttonStyle(.glassProminent)
-            
-            Text("Highscore: \(scoreManager.scores.first?.yourScore ?? 0)")
-            Spacer()
-            
-            Image(.jiachen)
-                .resizable()
-                .frame(width: CGFloat(100+2*counter), height: CGFloat(100+2*counter))
-                .padding()
-            
-            Spacer()
-            
-            Text("Swift progress: \(counter)")
-            
-            Button("I love Swift") {
-                counter += 1
-                if scoreManager.scores.first?.yourScore ?? 0 < counter {
-                    highscore = counter
-                    scoreManager.addNote(playerName: playerName, yourScore: highscore)
+        NavigationStack{
+            ZStack{
+                Image(.jiachen)
+                    .resizable()
+                    .frame(width: CGFloat(100+2*counter), height: CGFloat(100+2*counter))
+                
+                VStack {
+                    Button("Leaderboard") {
+                        showLeaderboard = true
+                    }
+                    .buttonStyle(.glassProminent)
+                    
+                    Text("Highscore: \(highscore)")
+                        .shadow(color:.white, radius: 1, x: 1, y:1)
+                        .shadow(color:.white, radius: 1, x: -1, y:1)
+                        .shadow(color:.white, radius: 1, x: 1, y: -1)
+                        .shadow(color:.white, radius: 1, x: -1, y:-1)
+                    
+                    Spacer()
+                    
+                    Text("Swift progress: \(counter)")
+                        .shadow(color:.white, radius: 1, x: 1, y:1)
+                        .shadow(color:.white, radius: 1, x: -1, y:1)
+                        .shadow(color:.white, radius: 1, x: 1, y: -1)
+                        .shadow(color:.white, radius: 1, x: -1, y:-1)
+                    
+                    Button("I love Swift") {
+                        counter += 1
+                        if highscore < counter {
+                            highscore = counter
+                            scoreManager.addNote(playerName: playerName, yourScore: highscore)
+                        }
+                    }
+                    .buttonStyle(.glassProminent)
+                    .sheet(isPresented: $showLeaderboard) {
+                        ClickerLeaderboardView(scoreManager: ScoreManager())
+                    }
+                    .onAppear {
+                        scoreManager.getNotes()
+                        if playerName.isEmpty {
+                            showNamePrompt = true
+                        }
+                    }
+                    .alert("Enter your name", isPresented: $showNamePrompt) {
+                        TextField("Your name", text: $playerName) // saves to @AppStorage as they type
+                        Button("Done") { showNamePrompt = false }
+                    }
                 }
             }
-                .buttonStyle(.glassProminent)
-            NavigationLink("Switch to player", destination: ClickerView(scoreManager: ScoreManager()))
-        }
-        .sheet(isPresented: $showLeaderboard) {
-            ClickerLeaderboardView(scoreManager: ScoreManager())
-        }
-        .padding()
-        .onAppear {
-            scoreManager.getNotes()
-            if playerName.isEmpty {
-                showNamePrompt = true
-            }
-        }
-        .alert("Enter your name", isPresented: $showNamePrompt) {
-            TextField("Your name", text: $playerName) // saves to @AppStorage as they type
-            Button("Done") { showNamePrompt = false }
         }
     }
 }
 
 #Preview {
-    ClickerView(scoreManager: ScoreManager())
+    SecondPlayerView(scoreManager: ScoreManager())
 }
